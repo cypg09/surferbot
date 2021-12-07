6aa1ab9a73bb8e9bc671ddb3572849a8c9c7ebc9
cypg09 <cypg09@protonmail.com>
2021-12-07 23:05:12 +0100

    Added footbar

---
+++ new-surferbot/src/Footbar/Footbar.js
@@ -0,0 +1,9 @@
+function Footbar() {
+  return (
+    <div class="container mx-auto bg-gray-600 rounded-xl p-3 m-3">
+      <p class="text-white text-center">2021 copyright Footbar</p>
+    </div>
+  );
+}
+
+export default Footbar;
